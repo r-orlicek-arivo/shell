1af091d66344cb03e6c5c2e37811ba803d873fce
Philipp Reitter <p.reitter@gmail.com>
2018-07-08 01:33:05 +0200

    changes to support osc

--- get.sh
+++ get.sh
@@ -1,17 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -ex
 
 # create and go to a temp folder
 D=$(mktemp -d)
 cd $D
 
-# install required packages via custom ppas
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:fish-shell/release-2
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update 
-sudo apt-get install -y ansible curl unzip fish
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    # install required packages via custom ppas
+    sudo apt-get update
+    sudo apt-get install -y software-properties-common
+    sudo apt-add-repository ppa:fish-shell/release-2
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update 
+    sudo apt-get install -y ansible curl unzip fish
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    brew update
+    brew install ansible curl unzip fish
+fi
 
 # download and execute shell setup playbook
 curl -L https://github.com/rettier/shell/archive/master.zip -o shell.zip 
